77f5d9bb8aaae6d246fbe3312fd3e08234ffb6cd
jakub.mikulski <kuba.mikulski@gmail.com>
2018-11-16 17:47:00 +0100

    week 3: implemented Visualization.tile

--- src/main/scala/observatory/Interaction.scala
+++ src/main/scala/observatory/Interaction.scala
@@ -1,6 +1,6 @@
 package observatory
 
-import com.sksamuel.scrimage.{Image, Pixel}
+import com.sksamuel.scrimage.{ Image, Pixel }
 
 import scala.math._
 
@@ -13,27 +13,33 @@ object Interaction {
     * @param tile Tile coordinates
     * @return The latitude and longitude of the top-left corner of the tile, as per http://wiki.openstreetmap.org/wiki/Slippy_map_tilenames
     */
-  def tileLocation(tile: Tile): Location = {
-    val n = 2 ^ tile.zoom
-    val lon = tile.x.toDouble / n * 360 - 180
-    val lat = atan(sinh(Pi * (1 - 2 * tile.y.toDouble / n))) * 180 / Pi
-    Location(lat, lon)
-  }
+  def tileLocation(tile: Tile): Location = coordinatesToLocation(tile.x, tile.y, tile.zoom)
 
   /**
     * @param temperatures Known temperatures
-    * @param colors Color scale
-    * @param tile Tile coordinates
+    * @param colors       Color scale
+    * @param tile         Tile coordinates
     * @return A 256×256 image showing the contents of the given tile
     */
   def tile(temperatures: Iterable[(Location, Temperature)], colors: Iterable[(Temperature, Color)], tile: Tile): Image = {
-    ???
+    val width, height = 256
+    val pixels = for {
+      y <- 0 until height
+      x <- 0 until width
+    } yield {
+      val location = coordinatesToLocation(x.toDouble / width + tile.x, y.toDouble / height + tile.y, tile.zoom)
+      val temp = Visualization.predictTemperature(temperatures, location)
+      val color = Visualization.interpolateColor(colors, temp)
+      Pixel(color.red, color.green, color.blue, 127)
+    }
+    Image(width, height, pixels.toArray)
   }
 
   /**
     * Generates all the tiles for zoom levels 0 to 3 (included), for all the given years.
-    * @param yearlyData Sequence of (year, data), where `data` is some data associated with
-    *                   `year`. The type of `data` can be anything.
+    *
+    * @param yearlyData    Sequence of (year, data), where `data` is some data associated with
+    *                      `year`. The type of `data` can be anything.
     * @param generateImage Function that generates an image given a year, a zoom level, the x and
     *                      y coordinates of the tile and the data to build the image from
     */
@@ -42,6 +48,13 @@ object Interaction {
     generateImage: (Year, Tile, Data) => Unit
   ): Unit = {
     ???
+  }
+
+  private def coordinatesToLocation(x: Double, y: Double, zoom: Int): Location = {
+    val n = 2 ^ zoom
+    val lon = x / n * 360 - 180
+    val lat = atan(sinh(Pi * (1 - 2 * y / n))) * 180 / Pi
+    Location(lat, lon)
   }
 
 }
