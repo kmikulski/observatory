6e8578ecbb1b5e7cdc57b01ac72e59bd6b788f6d
jakub.mikulski <kuba.mikulski@gmail.com>
2018-11-16 03:33:38 +0100

    week 2: implemented predictTemperature

--- src/main/scala/observatory/Visualization.scala
+++ src/main/scala/observatory/Visualization.scala
@@ -1,6 +1,6 @@
 package observatory
 
-import com.sksamuel.scrimage.{Image, Pixel}
+import com.sksamuel.scrimage.{ Image, Pixel }
 
 /**
   * 2nd milestone: basic visualization
@@ -9,16 +9,27 @@ object Visualization {
 
   /**
     * @param temperatures Known temperatures: pairs containing a location and the temperature at this location
-    * @param location Location where to predict the temperature
+    * @param location     Location where to predict the temperature
     * @return The predicted temperature at `location`
     */
   def predictTemperature(temperatures: Iterable[(Location, Temperature)], location: Location): Temperature = {
-    ???
+    def p(d: Double): Double = d * d
+
+    temperatures.find(t => location.distance(t._1) <= 1) match {
+      case Some(matched) =>
+        matched._2
+      case None =>
+        val (numerator, denominator) = temperatures.foldLeft((0.0, 0.0)) { (acc, x) =>
+          val w = 1 / p(location.distance(x._1))
+          (acc._1 + w * x._2, acc._1 + w)
+        }
+        numerator / denominator
+    }
   }
 
   /**
     * @param points Pairs containing a value and its associated color
-    * @param value The value to interpolate
+    * @param value  The value to interpolate
     * @return The color that corresponds to `value`, according to the color scale defined by `points`
     */
   def interpolateColor(points: Iterable[(Temperature, Color)], value: Temperature): Color = {
@@ -27,7 +38,7 @@ object Visualization {
 
   /**
     * @param temperatures Known temperatures
-    * @param colors Color scale
+    * @param colors       Color scale
     * @return A 360×180 image where each pixel shows the predicted temperature at its location
     */
   def visualize(temperatures: Iterable[(Location, Temperature)], colors: Iterable[(Temperature, Color)]): Image = {
