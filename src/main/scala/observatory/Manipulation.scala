36eabde63f8875fa7254c1808d83377177877c81
jakub.mikulski <kuba.mikulski@gmail.com>
2018-11-17 02:51:09 +0100

    week 4: implemented Manipulation

--- src/main/scala/observatory/Manipulation.scala
+++ src/main/scala/observatory/Manipulation.scala
@@ -10,27 +10,24 @@ object Manipulation {
     * @return A function that, given a latitude in [-89, 90] and a longitude in [-180, 179],
     *         returns the predicted temperature at this location
     */
-  def makeGrid(temperatures: Iterable[(Location, Temperature)]): GridLocation => Temperature = {
-    ???
-  }
+  def makeGrid(temperatures: Iterable[(Location, Temperature)]): GridLocation => Temperature =
+    (loc: GridLocation) => Visualization.predictTemperature(temperatures, Location(loc.lat, loc.lon))
 
   /**
     * @param temperaturess Sequence of known temperatures over the years (each element of the collection
     *                      is a collection of pairs of location and temperature)
     * @return A function that, given a latitude and a longitude, returns the average temperature at this location
     */
-  def average(temperaturess: Iterable[Iterable[(Location, Temperature)]]): GridLocation => Temperature = {
-    ???
-  }
+  def average(temperaturess: Iterable[Iterable[(Location, Temperature)]]): GridLocation => Temperature =
+    (loc: GridLocation) => temperaturess.map(t => makeGrid(t)(loc)).sum / temperaturess.size
 
   /**
     * @param temperatures Known temperatures
     * @param normals A grid containing the “normal” temperatures
     * @return A grid containing the deviations compared to the normal temperatures
     */
-  def deviation(temperatures: Iterable[(Location, Temperature)], normals: GridLocation => Temperature): GridLocation => Temperature = {
-    ???
-  }
+  def deviation(temperatures: Iterable[(Location, Temperature)], normals: GridLocation => Temperature): GridLocation => Temperature =
+    (loc: GridLocation) => makeGrid(temperatures)(loc) - normals(loc)
 
 
 }
