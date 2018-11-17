23f8b269a8430cedee1f442ec205e23cf85cd250
jakub.mikulski <kuba.mikulski@gmail.com>
2018-11-17 02:12:19 +0100

    week 3: added unit tests for Interaction.generateTiles

--- src/test/scala/observatory/InteractionTest.scala
+++ src/test/scala/observatory/InteractionTest.scala
@@ -3,8 +3,16 @@ package observatory
 import org.scalatest.FunSuite
 import org.scalatest.prop.Checkers
 
-import scala.collection.concurrent.TrieMap
-
 trait InteractionTest extends FunSuite with Checkers {
+
+  test("'generateTiles' should cover all expected tiles") {
+    var acc = List[(Year, Tile, String)]()
+    val yearlyData: Seq[(Year, String)] = Seq((2001, "first"), (2002, "second"))
+    def generateImage(year: Year, tile: Tile, s: String): Unit = {
+      acc = (year, tile, s) :: acc
+    }
+    Interaction.generateTiles(yearlyData, generateImage)
+    assert(acc.size == 170)
+  }
 
 }
